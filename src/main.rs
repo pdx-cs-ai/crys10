b26c4312f76cc789333fad3698a7c5ea6ff137ae
Bart Massey <bart.massey@gmail.com>
2020-10-26 01:28:36 -0700

    finished triple-finding code

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use io::BufRead;
 use collections::HashSet as Set;
 
 struct Word {
+    #[allow(unused)]
     word: String,
     letters: Set<char>,
     len: usize,
@@ -12,7 +13,6 @@ fn collect_words<T>(dict: io::BufReader<T>, ns: impl Iterator<Item=usize>) -> Ve
     where T: io::Read
 {
     let sizes: Set<usize> = ns.collect();
-    println!("{:?}", sizes);
     dict
         .lines()
         .map(|line| line.unwrap())
@@ -20,7 +20,6 @@ fn collect_words<T>(dict: io::BufReader<T>, ns: impl Iterator<Item=usize>) -> Ve
         .map(|word| {
             let letters = word.chars().collect();
             let len = word.len();
-            let word = word.to_string();
             Word { word, letters, len }
         })
         .collect()
@@ -31,5 +30,41 @@ fn main() {
         fs::File::open("/usr/local/share/dict/freq.txt").unwrap(),
     );
     let words = collect_words(dict, 10..=11);
-    println!("{}", words.len());
+
+    let mut ntrips = 0;
+    for (i, w1) in words.iter().enumerate() {
+        if w1.len != 10 {
+            continue;
+        }
+        for (j, w2) in words[i+1..].iter().enumerate() {
+            if w2.len != 10 {
+                continue;
+            }
+            let letters: Set<char> =
+                w1.letters.union(&w2.letters).cloned().collect();
+            if letters.len() > 10 {
+                continue;
+            }
+            for (k, w3) in words.iter().enumerate() {
+                if i == k || j == k {
+                    continue;
+                }
+                let nletters = letters.union(&w3.letters).count();
+                if nletters <= 10 {
+                    ntrips += 1;
+                    if ntrips % 1000000 == 0 {
+                        println!(
+                            "{} {} {} {}",
+                            ntrips,
+                            w1.word,
+                            w2.word,
+                            w3.word
+                        );
+                    }
+                }
+            }
+        }
+    }
+
+    println!("{}", ntrips);
 }
